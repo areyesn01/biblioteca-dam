550c252f4e7bcd79c0503e9fde55f3a44842d52c
aereyesn01 <aereyesn01@iesalbarregas.es>
2026-03-14 09:20:29 +0100

    Creacion del proyecto inicial con clases Libro, Biblioteca y Main

---
+++ Biblioteca.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Biblioteca {
+
+        private LinkedList<Libro> libros;
+
+        public Biblioteca() {
+            this.libros = new LinkedList<>();
+        }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Biblioteca biblioteca = new Biblioteca();
+
+        System.out.println("Sistema de biblioteca iniciado");
+
+    }
+}
